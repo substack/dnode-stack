a62743b3f7f847f38f4d0809ea1958faf971f6c4
James Halliday <substack@gmail.com>
2011-01-28 03:19:37 +0000

    check for req.socket

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var Seq = require('seq');
 module.exports = function (cb) {
     return function (client, conn) {
         var req = conn.stream.socketio.request;
-        if (conn.stream.socketio && req.socket.server) {
+        if (conn.stream.socketio && req.socket && req.socket.server) {
             conn.on('ready', function () {
                 Seq.ap(req.socket.server.stack || [])
                     .seqEach(function (mid) {
